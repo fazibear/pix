a6a8174563bf338d785fa8c742f6108fa4f8b330
Michał Kalbarczyk <fazibear@gmail.com>
2023-01-18 16:13:49 +0100

    cleanups

--- pix_elixir/apps/terminal/lib/terminal/application.ex
+++ pix_elixir/apps/terminal/lib/terminal/application.ex
@@ -4,21 +4,17 @@ defmodule Terminal.Application do
   @moduledoc false
 
   use Application
-  import Supervisor.Spec
 
   def start(_type, _args) do
-    # List all child processes to be supervised
-
     children = [
-      worker(Terminal, [nil])
+      Terminal
     ]
 
-    # Starts a worker by calling: Matrix.Worker.start_link(arg)
-    # {Matrix.Worker, arg},
+    opts = [
+      strategy: :one_for_one,
+      name: Terminal.Supervisor
+    ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Terminal.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
